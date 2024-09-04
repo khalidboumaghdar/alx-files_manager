bbb10f77d8c75f6dbe2e2b06fb62c95bd5f4c249
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2024-09-04 09:38:42 -0700

    khalid

--- server.js
+++ server.js
@@ -1,30 +1,15 @@
 import express from 'express';
-import controllerRouting from './routes/index';
+import router from './routes/index';
 
-/**
- * This project is a summary of back-end concepts:
- * authentication, NodeJS, MongoDB, Redis,
- * pagination and background processing.
- *
- * The objective was to build a simple platform to upload and view files:
- *
- * User authentication via a token
- * List all files
- * Upload a new file
- * Change permission of a file
- * View a file
- * Generate thumbnails for images
- */
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
-controllerRouting(app);
+app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
 
 export default app;
